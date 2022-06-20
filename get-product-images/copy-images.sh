02ab8df3967aeaa23424b1a5e6ad8c781d063a16
Simone De Nadai <simone.denada@gmail.com>
2022-06-20 10:20:30 +0200

    fix copy-images script

--- get-product-images/copy-images.sh
+++ get-product-images/copy-images.sh
@@ -1,5 +1,6 @@
 while IFS="" read -r p || [ -n "$p" ]
 do
-    rclone copy development:$p sandbox:$p
+    PARENT_DIR=$(dirname $p)
+    rclone copy development:$p sandbox:$PARENT_DIR
     echo "Done $p"
 done < images-list.txt
